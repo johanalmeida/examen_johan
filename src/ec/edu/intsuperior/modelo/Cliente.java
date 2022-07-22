fc0540ebba1b47a9dfd438f37ad1746ef0e7552d
ADMIN <ADMIN@DESKTOP-GQOTA9D>
2022-07-21 19:08:55 -0500

    EXAMEN

---
+++ src/ec/edu/intsuperior/modelo/Cliente.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Cliente {
+     private String telefono;
+
+    public Cliente() {
+    }
+
+    public Cliente(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+
+}
---
+++ src/ec/edu/intsuperior/modelo/Empleados.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Empleados {
+    private double sueldo;
+
+    public Empleados() {
+    }
+
+    public Empleados(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+
+    
+}
